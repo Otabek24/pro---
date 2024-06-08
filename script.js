2f0edeaf151fd68bf865c74318d71c121d13578f
Otabek <Otabek>
2024-06-08 14:14:25 +0500

    42f

---
+++ script.js
@@ -0,0 +1,35 @@
+let name = prompt('Ismingizni kiriting')
+console.log('Sizning ismingiz ' + name)
+let age = prompt('Yoshingizni kiriting')
+console.log('Sizning yoshingiz ' + age + 'da')
+let question = prompt('6+3=?')
+if (9==question) {
+    console.log("Sizning javobingiz to'g'ri")
+}else{
+    console.log("Sizning javobingiz noto'g'ri: " + question + ". To'g'ri javob:6+3=9")
+}
+let question2 = prompt('10-3=?')
+if (7==question2) {
+    console.log("Sizning javobingiz to'g'ri")
+}else{
+    console.log("Sizning javobingiz noto'g'ri: " + question2 + ". To'g'ri javob:10-3=7")
+}
+let question3 = prompt('12/3=?')
+if (4==question3) {
+    console.log("Sizning javobingiz to'g'ri")
+}else{
+    console.log("Sizning javobingiz noto'g'ri: " + question3 + ". To'g'ri javob:12/3=4")
+}
+let question4 = prompt('9*9=?')
+if (81==question4) {
+    console.log("Sizning javobingiz to'g'ri")
+}else{
+    console.log("Sizning javobingiz noto'g'ri: " + question4 + ". To'g'ri javob:9*9=81")
+}
+let question5 = prompt('13%2=?')
+if (1==question5) {
+    console.log("Sizning javobingiz to'g'ri")
+}else{
+    console.log("Sizning javobingiz noto'g'ri: " + question5 + ". To'g'ri javob:13%2=1")
+}
+alert('Konsolni ochib tekshirishingiz mumkin')
